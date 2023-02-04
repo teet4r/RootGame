21e14ffa09731848ef86c3f8ddabc9dc56209c85
teet4r <76508241+teet4r@users.noreply.github.com>
2023-02-04 15:24:33 +0900

    아군 오브젝트 배치 완료

--- Assets/KHJ/Scripts/UI/ButtonsManager.cs
+++ Assets/KHJ/Scripts/UI/ButtonsManager.cs
@@ -5,16 +5,45 @@ public class ButtonsManager : MonoBehaviour
 {
     public static ButtonsManager instance = null;
 
-    public Button[] fishButtons = null;
-    public Image[] fishImages = null;
+    [SerializeField] Text[] _fishCostTexts = null;
+    [SerializeField] Image[] _fishImages = null;
 
-    [Header("----- 5개 버튼의 팥/슈크림 순서 -----")]
-    public AllyFish[] redBeanButtonArray = null;
-    public AllyFish[] custardCreamButtonArray = null;
-
+    // 팥 순서, 슈크림 순서가 정해져 있음
+    public readonly AllyFish[] redBeans =
+    {
+        AllyFish.MiniFish_RB,
+        AllyFish.GoldFish,
+        AllyFish.BurntFish_RB,
+        AllyFish.TaiyakiFish_RB,
+        AllyFish.AgariFish_RB
+    };
+    public readonly AllyFish[] custardCreams =
+    {
+        AllyFish.MiniFish_CC,
+        AllyFish.GoldFish,
+        AllyFish.BurntFish_CC,
+        AllyFish.TaiyakiFish_CC,
+        AllyFish.AgariFish_CC
+    };
+    
     void Awake()
     {
         if (instance == null)
             instance = this;
+    }
+    void Start()
+    {
+        // 임시로 설정
+        SetButtons(redBeans);
+    }
+
+    public void SetButtons(params AllyFish[] fishes)
+    {
+        for (int i= 0; i < fishes.Length; i++)
+        {
+            var data = DataManager.instance.allyDatas[(int)fishes[i]];
+            _fishImages[i].sprite = data.Sprite;
+            _fishCostTexts[i].text = $"{data.Cost}";
+        }
     }
 }
