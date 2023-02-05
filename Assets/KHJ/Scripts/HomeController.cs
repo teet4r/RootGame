816131968b321d20507c90ce593b4c490687cd12
teet4r <76508241+teet4r@users.noreply.github.com>
2023-02-05 10:33:02 +0900

    본진 체력 추가

--- Assets/KHJ/Scripts/HomeController.cs
+++ Assets/KHJ/Scripts/HomeController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HomeController : MonoBehaviour
 {
@@ -6,15 +7,27 @@ public class HomeController : MonoBehaviour
     public int maxHp = 1000;
     public int curHp;
 
+    [SerializeField] Text _hpText = null;
+
     void Start()
     {
         curHp = maxHp;
+
+        _UpdateHpText();
     }
 
     public void GetDamage(int damage)
     {
         curHp = Mathf.Max(curHp - damage, 0);
+        _UpdateHpText();
         if (curHp <= 0)
             Destroy(gameObject);
+    }
+    
+    void _UpdateHpText()
+    {
+        var ratio = (float)curHp / maxHp;
+        _hpText.color = new Color(1f - ratio, ratio, 0f);
+        _hpText.text = $"{curHp}";
     }
 }
